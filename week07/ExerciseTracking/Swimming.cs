0f3fd2ce0f9f229787d2dc8ca7f615f2cfd233fa
An-Lu-En <annaluciaenyidiya@gmail.com>
2025-12-10 16:56:17 +0100

    week07

---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,27 @@
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        // Convert meters to miles: 1 mile = 1609 m
+        double totalMeters = _laps * 50;
+        return totalMeters / 1609.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
